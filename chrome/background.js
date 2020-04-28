65e9ca73aa2d5908648179579bac3817b220b6c7
Alex <penzov@cleveroad.com>
2020-04-28 23:05:57 +0300

    fixed bug refresh page

--- chrome/background.js
+++ chrome/background.js
@@ -1,73 +1,99 @@
-const installedTabs = {};
+const state = {};
+const initialTabState = {
+  toggle: false,
+  status: 'off',
+  injected: false
+}
+
+const set = (tabId, data) => {
+  state[tabId] = {
+    ...(state[tabId] || null),
+    ...data
+  }
+}
 
 const injectJS = (tab) => {
 
-  chrome.tabs.insertCSS({
+  if (state[tab.id] && state[tab.id].injected) {
+    return
+  }
+
+
+  chrome.tabs.insertCSS(tab.id, {
     file: 'all.css'
   });
 
-  chrome.tabs.executeScript({
+  chrome.tabs.executeScript(tab.id, {
     file: 'page.js'
   });
 
-  chrome.tabs.executeScript({
+  chrome.tabs.executeScript(tab.id, {
     file: 'runtime.js'
   });
 
-  chrome.tabs.executeScript({
+  chrome.tabs.executeScript(tab.id, {
     file: 'chunk.js'
   });
 
-  chrome.tabs.executeScript({
+  chrome.tabs.executeScript(tab.id, {
     file: 'main.js'
   });
 
-  installedTabs[tab.id] = {
-    visible: true,
-    url: tab.url
-  };
+  set(
+    tab.id, { injected: true }
+  )
+
 }
 
-chrome.tabs.onUpdated.addListener(function(tabId, _, tab){
-  if (tab.status !== 'complete') {
-    return;
+
+function set_status(tab) {
+  let { toggle, ...props } = state[tab.id] || initialTabState;
+  toggle = !toggle;
+
+  state[tab.id] = {
+    ...props,
+    toggle,
+    status: toggle ? 'on' : 'off'
   }
+}
 
-  if (installedTabs[tabId] && tab.url === installedTabs[tabId].url && installedTabs[tabId].visible) {
+function toggle_extension(tab) {
+  injectJS(tab);
 
-    injectJS(tab);
-    return;
-  }
-
-  installedTabs[tabId] = null;
-});
-
-
-chrome.browserAction.onClicked.addListener(function (tab) {
-
-  if (!installedTabs[tab.id]) {
-    injectJS(tab);
-
-    installedTabs[tab.id] = {
-      visible: true,
-      url: tab.url
-    };
-
-    return;
-  }
-
-
-  installedTabs[tab.id].visible = !installedTabs[tab.id].visible;
-
-  chrome.tabs.executeScript({
+  chrome.tabs.executeScript(tab.id, {
     code: `
     ;(function() {
       const frame = document.querySelector('#react-app-ext');
     
       if (frame) {
-        frame.style.display =  ${!!installedTabs[tab.id].visible} ? '' : 'none'
+        frame.style.display = frame.style.display ? '' : 'none'
       }
     })();
   `
   });
+  // Set the tab id
+  // the_tab_id = tab.id;
+  //state[tab.id] = initialTabState
+}
+
+function my_listener(tabId, changeInfo, tab) {
+  // If updated tab matches this one
+  if (changeInfo.status === "complete" &&
+    state[tabId] &&
+    state[tabId].status === 'on'
+  ) {
+    set(
+      tabId, { injected: false }
+    )
+    toggle_extension(tab);
+  }
+}
+
+chrome.browserAction.onClicked.addListener(function (tab) {
+
+  set_status(tab);
+  toggle_extension(tab);
 });
+
+chrome.tabs.onUpdated.addListener(my_listener);
+
